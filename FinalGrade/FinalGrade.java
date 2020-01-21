4c747e082fde009b6d67a839290d0cde9ca21f65
w0441213 <w0441213@nscc.ca>
2020-01-21 14:56:48 -0400

    WORKING WITH FOR LOOP

--- FinalGrade/FinalGrade.java
+++ FinalGrade/FinalGrade.java
@@ -10,78 +10,31 @@ public class FinalGrade {
     public static void main(String[] args) {
 
         // grade variables
-        float assignment1;
-        float assignment2;
-        float assignment3;
-        float assignment4;
-        float assignment5;
-        float assignment6;
+        float assignment;
 
         Scanner s = new Scanner(System.in);
 
-        System.out.print("Enter grade for assignment 1: " );
-        assignment1 = s.nextFloat();
-        System.out.print("Enter grade for assignment 2: " );
-        assignment2 = s.nextFloat();
-        System.out.print("Enter grade for assignment 3: " );
-        assignment3 = s.nextFloat();
-        System.out.print("Enter grade for assignment 4: " );
-        assignment4 = s.nextFloat();
-        System.out.print("Enter grade for assignment 5: " );
-        assignment5 = s.nextFloat();
-        System.out.print("Enter grade for assignment 6: " );
-        assignment6 = s.nextFloat();
+        //loop 6 times to get all assignment grades
+        float finalGrade=0.0f;
+        float lowest = 100.0f;
 
-        float finalGrade;
+        for(int i=0; i<6; i++) {
+            System.out.print("Enter grade for assignment " + (i+1) +": ");
+            assignment = s.nextFloat();
 
-        // test if user chooses the optional assignment
-        if (assignment6 >= 0) {
-            // if yes, determine lowest previous assignment
-            // and replace it
-            float lowest = 100.0f;
+            finalGrade +=assignment;
 
-            if (assignment1 < lowest) {
-                lowest = assignment1;
+            if(assignment < lowest) {
+                lowest = assignment;
             }
-            if (assignment2 < lowest) {
-                lowest = assignment2;
-            }
-            if (assignment3 < lowest) {
-                lowest = assignment3;
-            }
-            if (assignment4 < lowest) {
-                lowest = assignment4;
-            }
-            if (assignment5 < lowest) {
-                lowest = assignment5;
-            }
-
-            finalGrade =  assignment6;
-            if (assignment1 != lowest) {
-                finalGrade += assignment1;
-            }
-            if (assignment2 != lowest) {
-                finalGrade += assignment2;
-            }
-            if (assignment3 != lowest) {
-                finalGrade += assignment3;
-            }
-            if (assignment4 != lowest) {
-                finalGrade += assignment4;
-            }
-            if (assignment5 != lowest) {
-                finalGrade += assignment5;
-                //finalGrade = finalGrade + assignment5;
-            }
-            finalGrade /= 5.0f;
-
-        } else {
-
-            finalGrade =
-                    (assignment1 + assignment2 + assignment3 + assignment4 + assignment5 /* + assignment6 */) / 5;
 
         }
+
+        finalGrade -= lowest;
+        finalGrade /= 5.0f;
+
         System.out.println("Your final grade is: " + finalGrade);
 
     }
 }
+
