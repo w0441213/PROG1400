8950b5ae8bb07d469f4629d9e4112d9c4128b0db
w0441213 <w0441213@nscc.ca>
2020-01-23 10:55:15 -0400

    started adding in arrays

--- FinalGrade/FinalGrade.java
+++ FinalGrade/FinalGrade.java
@@ -4,37 +4,21 @@ import java.util.Scanner;
 
 public class FinalGrade {
 
-    // Write a program that asks the user to enter three test scores.
-    // The program should display each test score, as well as the average of the scores.
-
     public static void main(String[] args) {
 
         // grade variables
-        float assignment;
+        float[] assignments = new float[6];
 
         Scanner s = new Scanner(System.in);
 
-        //loop 6 times to get all assignment grades
-        float finalGrade=0.0f;
+        // loop 6 times to get all assignment grades
+        float finalGrade = 0.0f;
         float lowest = 100.0f;
-
-        for(int i=0; i<6; i++) {
-            System.out.print("Enter grade for assignment " + (i+1) +": ");
-            assignment = s.nextFloat();
-
-            finalGrade +=assignment;
-
-            if(assignment < lowest) {
-                lowest = assignment;
-            }
-
+        for (int i=0; i<6; i++) {
+            System.out.print("Enter grade for assignment " + (i+1) + ": ");
+            assignments[i] = s.nextFloat();
         }
 
-        finalGrade -= lowest;
-        finalGrade /= 5.0f;
-
         System.out.println("Your final grade is: " + finalGrade);
-
     }
 }
-
